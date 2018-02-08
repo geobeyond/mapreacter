04934a63066f24b207b1dc91c25c060b43781b4c
StefanoMorroni <smorroni@hotmail.com>
2018-02-08 15:10:03 +0100

    material ui ispraTheme

--- src/client.jsx
+++ src/client.jsx
@@ -46,39 +46,35 @@ export const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunkMiddleware));
 
-const ispraTheme =
-  {
-    spacing: {
-      iconSize: 24,
-      desktopGutter: 24,
-      desktopGutterMore: 32,
-      desktopGutterLess: 16,
-      desktopGutterMini: 8,
-      desktopKeylineIncrement: 64,
-      desktopDropDownMenuItemHeight: 32,
-      desktopDropDownMenuFontSize: 15,
-      desktopDrawerMenuItemHeight: 48,
-      desktopSubheaderHeight: 48,
-      desktopToolbarHeight: 56
-    },
-    fontFamily: 'Roboto, sans-serif',
-    palette: {
-      primary1Color: '#0097a7',
-      primary2Color: '#0097a7',
-      primary3Color: '#757575',
-      accent1Color: '#ff4081',
-      accent2Color: '#f50057',
-      accent3Color: '#ff80ab',
-      textColor: 'rgba(255, 255, 255, 1)',
-      secondaryTextColor: 'rgba(255, 255,  255, 0.7)',
-      alternateTextColor: '#303030',
-      canvasColor: '#303030',
-      borderColor: 'rgba(255, 255, 255, 0.3)',
-      disabledColor: 'rgba(255, 255, 255, 0.3)',
-      pickerHeaderColor: 'rgba(255, 255, 255, 0.12)',
-      clockCircleColor: 'rgba(255, 255, 255, 0.12)'
-    }
-  }
+import {
+  cyan500, cyan700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+import { fade } from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
+
+const ispraTheme = {
+  spacing: spacing,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: cyan500,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
+    accent2Color: '#00601d', //grey100,
+    accent3Color: grey500,
+    textColor: white, //darkBlack,
+    alternateTextColor: white,
+    canvasColor: '#00601d', //white,
+    borderColor: grey300,
+    disabledColor: fade(white, 0.3), //fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+};
 
 class Client {
   constructor(mapId, config = {}) {
