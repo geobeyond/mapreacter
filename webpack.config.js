2b22596420987b26769a7ee9fb208a09a6f702a5
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-02-13 23:49:06 +0100

    Remove webpack-dashboard to fix #189

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,22 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HTMLWebpackPlugin = require("html-webpack-plugin");
 var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 var Minimist = require("minimist");
-var DashboardPlugin = require("webpack-dashboard/plugin");
 
 var BUILD_DIR = path.resolve(__dirname, "dist");
 var APP_DIR = path.resolve(__dirname, ".");
 var DEFAULT_CONTEXT = "/mapreacter";
 
-var plugins = [
-	new webpack.ProvidePlugin({
-		Intl: "imports?this=>global!exports?global.Intl!intl"
-	})
-];
+var plugins = [new webpack.ProvidePlugin({Intl: "imports?this=>global!exports?global.Intl!intl"})];
 var filename = "[name].js";
 var devtool = "inline-source-map";
 if (process.env.NODE_ENV === "development") {
-	plugins.push(
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("development")
-			}
-		})
-	);
-	plugins.push(
-		new ExtractTextPlugin({ filename: "css/[name].css", disable: false })
-	);
+	plugins.push(new webpack.DefinePlugin({
+		"process.env": {
+			NODE_ENV: JSON.stringify("development")
+		}
+	}));
+	plugins.push(new ExtractTextPlugin({filename: "css/[name].css", disable: false}));
 	plugins.push(new ExtractTextPlugin("css/sdk.css"));
-	plugins.push(new DashboardPlugin());
 	filename = "[name].js";
 	devtool = "";
 
@@ -62,34 +52,28 @@ if (process.env.NODE_ENV === "development") {
 					test: /\.(js|jsx)$/,
 					loader: "babel-loader",
 					exclude: /node_modules/
-				},
-				{
+				}, {
 					test: /\.less$/,
 					use: ExtractTextPlugin.extract({
 						fallback: "style-loader",
 						use: [
-							"css-loader",
-							{
+							"css-loader", {
 								loader: "less-loader"
 							}
 						]
 					})
-				},
-				{
+				}, {
 					test: /\.json$/,
 					loader: "json-loader"
-				},
-				{
+				}, {
 					test: /\.(png|gif|jpg|jpeg|svg|otf|ttf|eot|woff)$/,
 					loader: "file-loader"
-				},
-				{
+				}, {
 					test: /\.s?css$/,
 					use: ExtractTextPlugin.extract({
 						fallback: "style-loader",
 						use: [
-							"css-loader",
-							{
+							"css-loader", {
 								loader: "sass-loader",
 								options: {
 									includePaths: ["node_modules"]
@@ -110,37 +94,26 @@ if (process.env.NODE_ENV === "development") {
 	};
 }
 if (process.env.NODE_ENV === "production") {
-	plugins.push(
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("production")
-			}
-		})
-	);
-	plugins.push(
-		new ExtractTextPlugin({
-			filename: "css/[name].min.css",
-			disable: false
-		})
-	);
+	plugins.push(new webpack.DefinePlugin({
+		"process.env": {
+			NODE_ENV: JSON.stringify("production")
+		}
+	}));
+	plugins.push(new ExtractTextPlugin({filename: "css/[name].min.css", disable: false}));
 	plugins.push(new ExtractTextPlugin("css/sdk.min.css"));
-	plugins.push(
-		new HTMLWebpackPlugin({
-			filename: "index.html",
-			template: path.resolve(APP_DIR, "src/tpl/prod.html"),
-			inject: "head"
-		})
-	);
-	plugins.push(
-		new UglifyJSPlugin({
-			uglifyOptions: {
-				sourceMap: true, // enable source maps to map errors (stack traces) to modules
-				output: {
-					comments: false // remove all comments
-				}
+	plugins.push(new HTMLWebpackPlugin({
+		filename: "index.html",
+		template: path.resolve(APP_DIR, "src/tpl/prod.html"),
+		inject: "head"
+	}));
+	plugins.push(new UglifyJSPlugin({
+		uglifyOptions: {
+			sourceMap: true, // enable source maps to map errors (stack traces) to modules
+			output: {
+				comments: false // remove all comments
 			}
-		})
-	);
+		}
+	}));
 	filename = "[name].min.js";
 	devtool = "";
 
@@ -148,9 +121,7 @@ if (process.env.NODE_ENV === "production") {
 		// get context command line argument eg: "webpack --context=mapreacter"
 		var context = "/" + Minimist(process.argv.slice(2)).context;
 		if (!context) {
-			console.log(
-				"No build context provided, using default target instead\n\n"
-			);
+			console.log("No build context provided, using default target instead\n\n");
 			context = defaultContext;
 		}
 		return context;
@@ -185,34 +156,28 @@ if (process.env.NODE_ENV === "production") {
 					test: /\.(js|jsx)$/,
 					loader: "babel-loader",
 					exclude: /node_modules/
-				},
-				{
+				}, {
 					test: /\.less$/,
 					use: ExtractTextPlugin.extract({
 						fallback: "style-loader",
 						use: [
-							"css-loader",
-							{
+							"css-loader", {
 								loader: "less-loader"
 							}
 						]
 					})
-				},
-				{
+				}, {
 					test: /\.json$/,
 					loader: "json-loader"
-				},
-				{
+				}, {
 					test: /\.(png|gif|jpg|jpeg|svg|otf|ttf|eot|woff)$/,
 					loader: "file-loader"
-				},
-				{
+				}, {
 					test: /\.s?css$/,
 					use: ExtractTextPlugin.extract({
 						fallback: "style-loader",
 						use: [
-							"css-loader",
-							{
+							"css-loader", {
 								loader: "sass-loader",
 								options: {
 									includePaths: ["node_modules"]
