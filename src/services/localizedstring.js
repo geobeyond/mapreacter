0dc047827d83d83752e7413792df4dfc967a58d3
StefanoMorroni <smorroni@hotmail.com>
2018-03-02 18:56:59 +0100

    set default language

--- src/services/localizedstring.js
+++ src/services/localizedstring.js
@@ -2,22 +2,24 @@
 import LocalizedStrings from 'react-localization';
 
 export const mylocalizedstrings = new LocalizedStrings({
-    it: {
-      selectLanguage: "lingua selezionata :",
-      tassonomialabel: "Tassonomia ...",
-      sharetitle: "per condividere la pagina, copia ed invia questo link:",
-      close: "CHIUDI",
-      phylumlabel: "Phylum",
-      famiglialabel: "Famiglia",
-      specielabel: "Specie",
-    },
-    en: {
-      selectLanguage: "language selected:",
-      tassonomialabel: "Taxonomy ...",
-      sharetitle: "in order to share the page, copy and send this link:",
-      close: "CLOSE",
-      phylumlabel: "Phylum",
-      famiglialabel: "Family",
-      specielabel: "Species",
-    },
-  });
+  it: {
+    selectLanguage: "lingua selezionata :",
+    tassonomialabel: "Tassonomia ...",
+    sharetitle: "per condividere la pagina, copia ed invia questo link:",
+    close: "CHIUDI",
+    phylumlabel: "Phylum",
+    famiglialabel: "Famiglia",
+    specielabel: "Specie",
+  },
+  en: {
+    selectLanguage: "language selected:",
+    tassonomialabel: "Taxonomy ...",
+    sharetitle: "in order to share the page, copy and send this link:",
+    close: "CLOSE",
+    phylumlabel: "Phylum",
+    famiglialabel: "Family",
+    specielabel: "Species",
+  },
+});
+
+mylocalizedstrings.setLanguage('it');  
