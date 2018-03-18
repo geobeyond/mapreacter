b3f040ad505075402991e9b7ac7ede039eeba8a4
StefanoMorroni <smorroni@hotmail.com>
2018-03-18 18:03:22 +0100

    the configuration is again outsourced

--- src/services/localizedstring.js
+++ src/services/localizedstring.js
@@ -2,31 +2,6 @@
 import LocalizedStrings from 'react-localization';
 require('dotenv').config();
 
-const theprops = JSON.parse(process.env.REACT_APP_MESSAGES);
-
-/*export const mylocalizedstrings = new LocalizedStrings({
-  it: {
-    selectLanguage: "lingua selezionata :",
-    tassonomialabel: "Tassonomia ...",
-    sharetitle: "per condividere la pagina, copia ed invia questo link:",
-    helptitle: "Guida in linea",
-    close: "CHIUDI",
-    phylumlabel: "Phylum",
-    famiglialabel: "Famiglia",
-    specielabel: "Specie",
-  },
-  en: {
-    selectLanguage: "language selected:",
-    tassonomialabel: "Taxonomy ...",
-    sharetitle: "in order to share the page, copy and send this link:",
-    helptitle: "Help on line",
-    close: "CLOSE",
-    phylumlabel: "Phylum",
-    famiglialabel: "Family",
-    specielabel: "Species",
-  },
-});*/
-
-export const mylocalizedstrings = new LocalizedStrings(theprops);
+export const mylocalizedstrings = new LocalizedStrings(window.config.messages);
 
 mylocalizedstrings.setLanguage('it');  
