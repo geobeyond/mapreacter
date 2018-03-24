c08c1821bc4ea5932786967db5bae1fd4d795f6d
StefanoMorroni <smorroni@hotmail.com>
2018-03-24 01:04:50 +0100

    TOC – poco chiara , mettere i nomi dei layer a sx , rimpicciolire testi ed icone, Utilizzare i chakbox

--- src/components/map/LayerListItem.jsx
+++ src/components/map/LayerListItem.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import { DragSource, DropTarget } from 'react-dnd';
 import { types, layerListItemSource, layerListItemTarget, collect, collectDrop } from '@boundlessgeo/sdk/components/layer-list-item';
 import SdkLayerListItem from '@boundlessgeo/sdk/components/layer-list-item';
+import SdkLegend from '@boundlessgeo/sdk/components/legend';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
-import { isLayerVisible } from '@boundlessgeo/sdk/util';
+//import { isLayerVisible } from '@boundlessgeo/sdk/util';
 import { fitextent } from '../../actions/map';
 
 
 class LayerListItem extends SdkLayerListItem {
   render() {
     const layer = this.props.layer;
-    //const checkbox = this.getVisibilityControl(layer);
-    const checkbox = (<i className={isLayerVisible(this.props.layer) ? 'fa fa-eye fa-lg' : 'fa fa-eye-slash fa-lg'} onClick={() => { this.toggleVisibility(); }} />);
+    const checkbox = this.getVisibilityControl(layer);
+    //const checkbox = (<i className={isLayerVisible(this.props.layer) ? 'fa fa-eye fa-lg' : 'fa fa-eye-slash fa-lg'} onClick={() => { this.toggleVisibility(); }} />);
 
     let fitextentbutton = null;
-    if (layer.id === 'osm' || layer.id === 'mapbox') {
-      fitextentbutton = (<span />);
-    } else {
+    if(layer.type !== "raster") {
       fitextentbutton = (
         <IconButton
           onClick={() => {
@@ -29,7 +28,7 @@ class LayerListItem extends SdkLayerListItem {
     }
 
     let moveButtons = (
-      <span>
+      <span className="btn-container">
         <IconButton
           onClick={() => {
             this.moveLayerUp();
@@ -46,11 +45,17 @@ class LayerListItem extends SdkLayerListItem {
       </span>
     );
 
+    let legend = null;
+    if(layer.type !== "raster") {
+      legend = (<SdkLegend style={{display:'inline'}} key={layer.id} layerId={layer.id} />);
+    }
+
     return this.props.connectDragSource(this.props.connectDropTarget((
       <li className="layer">
-        <span className="checkbox">{checkbox}</span>
-        <span className="btn-container">{moveButtons}</span>
         <span className="name">{layer.id}</span>
+        {checkbox}
+        {moveButtons}
+        {legend}
       </li>
     )));
   }
