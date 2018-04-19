1782978852b405447f829e93087f5e34b1038e1d
StefanoMorroni <smorroni@hotmail.com>
2018-04-19 16:01:34 +0200

    fix

--- src/actions/map.js
+++ src/actions/map.js
@@ -3,13 +3,6 @@ import { createWMSLayer, createWMSSourceWithLayerName } from '../services/wms/wm
 
 export const viewparams = [];
 
-export const changeOrAddViewParamsToUrl = (url, viewparams) => {
-  console.log("map.changeOrAddViewParamsToUrl()", url, viewparams);
-  if (url.search('viewparams') > 0) {
-    return url.replace(/(viewparams=).*?(&|$)/, '$1' + viewparams + '$2');
-  }
-  return url + '&viewparams=' + viewparams;
-}
 
 export const updateLayersWithViewparams = (params) => {
   console.log("map.updateLayersWithViewparams()", params);
@@ -17,12 +10,48 @@ export const updateLayersWithViewparams = (params) => {
     const { local } = getState();
 
     viewparams.length = 0;
-    params.forEach((param, i) => {
+    /*params.forEach((param, i) => {
       if (param !== '*') {
         let paramEscaped = param.replace(",", "\\,").replace(";", "\\;");
         viewparams.push(local.mapConfig.viewparams[i] + ':' + paramEscaped);
       }
-    });
+    });*/
+
+    let counter = 0;
+    for (let i = 0; i < 4; i++) {
+      if (params[i]) {
+        if (params[i] !== '*') {
+          let paramEscaped = params[i].replace(",", "\\,").replace(";", "\\;");
+          if (local.mapConfig.viewparams[i]) {
+            viewparams.push(local.mapConfig.viewparams[i] + ':' + paramEscaped);
+            counter++;
+          }
+        }
+      }
+    }
+    if (counter === 0) {
+      let _item = local.mapConfig.viewparams[0] + ':none';
+      console.log("map.updateLayersWithViewparams() aggiungo ", _item);
+      viewparams.push(_item);
+    }
+
+    counter = 0;
+    for (let i = 4; i < 8; i++) {
+      if (params[i]) {
+        if (params[i] !== '*') {
+          let paramEscaped = params[i].replace(",", "\\,").replace(";", "\\;");
+          if (local.mapConfig.viewparams[i]) {
+            viewparams.push(local.mapConfig.viewparams[i] + ':' + paramEscaped);
+            counter++;
+          }
+        }
+      }
+    }
+    if (counter === 0) {
+      let _item = local.mapConfig.viewparams[4] + ':none';
+      console.log("map.updateLayersWithViewparams() aggiungo ", _item);
+      viewparams.push(_item);
+    }
 
     local.mapConfig.layers.forEach((layerName, i, layers) => {
       let sourceUrl = encodeURI(local.mapConfig.source + '&viewparams=' + viewparams.join(';'));
