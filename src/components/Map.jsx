dce8b93fed11ba03180847bc2effb19c3e4eb35c
StefanoMorroni <smorroni@hotmail.com>
2018-05-15 20:49:31 +0200

    LegendControl ... first version

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,12 +7,54 @@ import SdkMap from '@boundlessgeo/sdk/components/map';
 import SdkMousePosition from '@boundlessgeo/sdk/components/map/mouseposition';
 import SdkScaleLine from '@boundlessgeo/sdk/components/map/scaleline';
 import * as printActions from '@boundlessgeo/sdk/actions/print';
+import ol from 'ol';
+import Control from 'ol/control/control';
 import OverviewMap from 'ol/control/overviewmap';
 //import FullScreen from 'ol/control/fullscreen';
 import WMSPopup from './map/wms/wmspopup'
 import ZoomControl from './map/zoom-control';
 import * as actions from '../actions/map';
 import { store } from '../App';
+
+const LegendControl = function (opt_options) {
+
+  var theInfo = document.createElement('div');
+  theInfo.innerHTML = '<p style="color:#00ffff">this is a simple string</p>';
+  theInfo.style.float = 'right';
+  theInfo.style.display = 'none';
+
+  var options = opt_options || {};
+
+  var theButton = document.createElement('button');
+  theButton.innerHTML = 'L';
+
+  //var this_ = this;
+  var handleLegendControl = function () {
+    console.log("handleLegendControl()", theInfo.style.display);
+    //this_.getMap().getView().setRotation(0);
+    if (theInfo.style.display === 'block' || theInfo.style.display === '') {
+      theInfo.style.display = 'none';
+    } else {
+      theInfo.style.display = 'block';
+    }
+  };
+
+  theButton.addEventListener('click', handleLegendControl, false);
+  theButton.addEventListener('touchstart', handleLegendControl, false);
+
+  var element = document.createElement('div');
+  element.className = 'legend-control ol-unselectable ol-control';
+  element.appendChild(theButton);
+  element.appendChild(theInfo);
+
+  Control.call(this, {
+    element: element,
+    target: options.target
+  });
+
+};
+
+ol.inherits(LegendControl, Control);
 
 class Map extends Component {
 
@@ -61,13 +103,14 @@ class Map extends Component {
             if (input) {
               input.wrappedInstance.map.addControl(new OverviewMap());
               //input.wrappedInstance.map.addControl(new FullScreen());
+              input.wrappedInstance.map.addControl(new LegendControl());
             }
           }}
           style={{ position: 'relative' }}
           accessToken={token}
           includeFeaturesOnClick
           onExportImage={this.exportMapImage}
-          onClick={(map, xy, featuresPromise) => {            
+          onClick={(map, xy, featuresPromise) => {
             this.props.changerefreshindicator({ status: "loading" });
             featuresPromise.then((featureGroups) => {
               let items = [];
@@ -87,7 +130,7 @@ class Map extends Component {
                     closeable
                     items={items}
                   />
-                );             
+                );
               }
               this.props.changerefreshindicator({ status: "hide" });
             });
