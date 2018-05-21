d4f0b8df2a060ac6f0186e79a51909e774e12c02
StefanoMorroni <smorroni@hotmail.com>
2018-05-21 17:02:16 +0200

    fix

--- src/components/map/LayerListItem.jsx
+++ src/components/map/LayerListItem.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { DragSource, DropTarget } from 'react-dnd';
 import { types, layerListItemSource, layerListItemTarget, collect, collectDrop } from '@boundlessgeo/sdk/components/layer-list-item';
 import SdkLayerListItem from '@boundlessgeo/sdk/components/layer-list-item';
-import SdkLegend from '@boundlessgeo/sdk/components/legend';
 import IconButton from 'material-ui/IconButton';
-//import { isLayerVisible } from '@boundlessgeo/sdk/util';
 import { fitextent } from '../../actions/map';
 
 
@@ -12,7 +10,6 @@ class LayerListItem extends SdkLayerListItem {
   render() {
     const layer = this.props.layer;
     const checkbox = this.getVisibilityControl(layer);
-    //const checkbox = (<i className={isLayerVisible(this.props.layer) ? 'fa fa-eye fa-lg' : 'fa fa-eye-slash fa-lg'} onClick={() => { this.toggleVisibility(); }} />);
 
     let fitextentbutton = null;
     if (layer.type !== "raster") {
@@ -43,11 +40,6 @@ class LayerListItem extends SdkLayerListItem {
         {fitextentbutton}
       </span>
     );
-
-    let legend = null;
-    if (layer.type !== "raster") {
-      legend = (<SdkLegend style={{ display: 'inline' }} key={layer.id} layerId={layer.id} />);
-    }
 
     return this.props.connectDragSource(this.props.connectDropTarget((
       <li className="sdk-layer">
